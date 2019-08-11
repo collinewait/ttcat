e2d7e578bc7ff0dfe782b3807728ae6a1c15e432
collinewait <collinewait17@gmail.com>
2019-08-11 14:54:43 +0300

    add a credentials manager

--- bin/ttcat.js
+++ bin/ttcat.js
@@ -1,3 +1,12 @@
 #! /usr/bin/env node
+const CredentialManager = require('../lib/credential-manager');
 
-console.log('Hello twitter..');
+async function main() {
+  const credentialManager = new CredentialManager('ttcat');
+
+  const [key, secret] = await credentialManager.getKeyAndSecret();
+
+  console.log(key, secret);
+}
+
+main().catch(console.error);
---
+++ tests/credential-manager.spec.js
@@ -0,0 +1,42 @@
+const chai = require('chai');
+const sinon = require('sinon');
+const inquirer = require('inquirer');
+const dirtyChai = require('dirty-chai');
+
+chai.use(dirtyChai);
+const { expect } = chai;
+
+const CredentialManager = require('../lib/credential-manager');
+
+describe('a credential manager', () => {
+  let credentialManager;
+  before(() => {
+    credentialManager = new CredentialManager('ttcat-test');
+  });
+
+  after(() => {
+    credentialManager.conf.delete('apiKey');
+    credentialManager.conf.delete('apiSecret');
+  });
+
+  context('with no existing credentials', () => {
+    it('should prompt the user', async () => {
+      sinon
+        .stub(inquirer, 'prompt')
+        .resolves({ key: 'testKey', secret: 'testSecret' });
+      const [key, secret] = await credentialManager.getKeyAndSecret();
+      expect(key).to.equal('testKey');
+      expect(secret).to.equal('testSecret');
+      expect(inquirer.prompt.calledOnce).to.be.true();
+      inquirer.prompt.restore();
+    });
+  });
+
+  context('with existing credentials', () => {
+    it('should just return them', async () => {
+      const [key, secret] = await credentialManager.getKeyAndSecret();
+      expect(key).to.equal('testKey');
+      expect(secret).to.equal('testSecret');
+    });
+  });
+});
